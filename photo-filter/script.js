a23ca2e74955cfd31fceb9f692ee056e6f616b1e
tkachiko <tkachikos@gmail.com>
2021-03-21 17:20:53 +0300

    feat: implement handler for input and output

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,11 +1,30 @@
-const fullscreen = document.querySelector(".fullscreen");
+const input = document.querySelectorAll('.filters input');
+const output = document.querySelectorAll('.filters output');
+const fullscreen = document.querySelector('.fullscreen');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+  
+  if (this.matches(`input[type="range"]`)) {
+    output.forEach(output => {
+      if (output.previousElementSibling.name === this.name)
+        output.value = this.value;
+    });
+  }
+}
 
 const togglefullscreen = () => {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen();
-    } else {
-      document.exitFullscreen();
-    }
-  };
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+};
 
-  fullscreen.addEventListener("click", togglefullscreen);
+input.forEach(input => input.addEventListener('input', handleUpdate));
+
+fullscreen.addEventListener('click', togglefullscreen);
